ef9c3205b2141cb51c10b070571f84147ff1cdee
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-11-13 00:59:13 -0500

    handle basic requests

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::io::prelude::{
+    Read,
+    Write
+};
+use std::net::{
+    TcpListener,
+    TcpStream
+};
+use std::fs::File;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+}
